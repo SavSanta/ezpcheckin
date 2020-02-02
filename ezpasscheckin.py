15cf4363978c2c2c3031a5b7b3a38498d6381a83
SavSanta <remethyst@yahoo.com>
2020-02-02 06:53:51 -0500

    Mostly done scaffolds. Work on making more functions

---
+++ ezpasscheckin.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import requests, bs4
+from random import randint
+
+
+def search_violations(format, goop):
+    ''' Preps data out to be sent in POST url form submission"
+
+    basedata = {
+        org.apache.struts.taglib.html.TOKEN: formtoken,
+        formid: frmViolationInquiry,
+        btnSearch.x: randint(0,70),
+        btnSearch.y: randint(0,18),
+        ctokenElem: formtoken,
+       }
+
+
+    return data
+
+
+def get_errflash(soup)"
+
+    return soup.form.div.table.tbody.td.table.tr.td.table.td.next.next.next.text
+
+def find_token(soup):
+
+    try:
+        token = soup.form.input.attrs['value']
+        return token
+    except Exception as e:
+        pass
+
+
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/79.0.3945.79 Chrome/79.0.3945.79 Safari/537.36"
+URL = "https://www.ezpassmd.com/vector/violations/violNoticeInquiry.do?locale=en_US&from=Home"
+
+
+token = find_token
+
+req = requests.request(URL, headers={"User-Agent' : USER_AGENT})
