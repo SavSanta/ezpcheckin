fb217b14494f7ffc93e4547137b92db81bbe1a27
SavSanta <remethyst@yahoo.com>
2020-02-02 07:23:14 -0500

    FUNCTION TODOs

--- ezpcheckin.py
+++ ezpcheckin.py
@@ -23,6 +23,7 @@ def get_errflash(soup)"
 
     return soup.form.div.table.tbody.td.table.tr.td.table.td.next.next.next.text
 
+
 def find_token(soup):
 
     try:
@@ -31,6 +32,10 @@ def find_token(soup):
     except Exception as e:
         pass
 
+
+# CREATE LOGGER DECORATOR HERE
+
+# CREATE REQUESTS.REQUEST DECORATOR HERE (NO IM NOT USING SESSION)
 
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/79.0.3945.79 Chrome/79.0.3945.79 Safari/537.36"
 URL = base64.decodestring(b"aHR0cHM6Ly93d3cuZXpwYXNzbWQuY29tL3ZlY3Rvci92aW9sYXRpb25zL3Zpb2xOb3RpY2VJbnF1aXJ5LmRvP2xvY2FsZT1lbl9VUyZmcm9tPUhvbWU=")
