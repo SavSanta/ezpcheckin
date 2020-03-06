67a4003319229d0edb8be3df72d2907606786d9c
SavSanta <remethyst@yahoo.com>
2020-03-06 17:49:27 -0500

    Major rewrite to version 2.0

--- ezpcheckin.py
+++ ezpcheckin.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+# ezpcheckin.py
+# Author: Savsanta (Ru Uba)
+# Version: 2.0
+# This script checks for kickback-tolls that may've not been mailed to you in this godforsaken expensive state and country.
+# This progam using f-strings and requires Python 3.6 or greater.
+
+
 import requests, bs4, base64, functools
 from random import randint
 
@@ -8,15 +15,37 @@ URL = base64.decodestring(b"aHR0cHM6Ly93d3cuZXpwYXNzbWQuY29tL3ZlY3Rvci92aW9sYXRp
 
 
 def check_resp(content):
-    pass
+    if content.status_code == 200:
+        print("Response Check returned OK!\n")
+        return
+    elif (400 <= content.status_code < 500):
+        print("Response Check returned failing 400 series.\n")
+        raise Exception("400 Server Error Code. Aborting")
+    elif (500 <= content.status_code < 600):
+        print("Response Check returned failing 500 series. Site is having issues.\n")
+        raise Exception("500 Server Error Code. Aborting")
+    else:
+        print(f"Unknown Error Code: {content.status_code}.\n")
+        raise Exception("Unknown Response Code. Aborting")
 
 
 def readstore(config="ezpstore.txt"):
-    ''' Reads the lines for the EZPass accounts to check and returns as a list variable '''
+    ''' Reads the lines for the EZPass accounts to check and returns non-blank/non-commented lines as a list variable '''
 
+    items = []
     with open(config) as lookup:
-        items = lookup.readlines()
-        return items
+        while True:
+            line = lookup.readline()
+
+            # Strip out newlines and comments
+            if (line.lstrip().startswith('#')) or (line == '\n'):
+                continue
+            elif (not line):
+               break
+            else:
+                items.append(line)
+
+    return items
 
 
 def request(*args, **kwargs):
@@ -51,20 +80,20 @@ def search_violation(cfgdata):
 
     basedata = {
         'org.apache.struts.taglib.html.TOKEN': formtoken,
-        'formid': frmViolationInquiry,
+        'formid': 'frmViolationInquiry',
         'btnSearch.x': randint(0,70),
         'btnSearch.y': randint(0,18),
         'ctokenElem': formtoken,
        }
 
-    basedata.append(cfgdata)
-    resp = request('GET', URL, data=basedata)
+    basedata.update(cfgdata)
+    resp = request('POST', URL, data=basedata)
 
     return resp
 
 
 def convcfg(data):
-    ''' Convert the data from configuration list into values to use in search violation. '''
+    ''' Convert the data from configuration list into values to use in the violation search and email dispatching. '''
 
     d = {}
 
@@ -73,7 +102,7 @@ def convcfg(data):
 
     if chunks[0] == 'PLATE':
         # Example values to populate
-        # loginNumber = 2DLXEFM or S123456789 or 555 Santa Claus Street | licenstate = MD | zipcode = 47450
+        # loginNumber = 2DLXEFM or S123456789 or L7564334578 | licenstate = MD | zipcode = 47450
         d.update({'loginType': 'plate', 'selectCreditCard':'new', 'loginNumber':chunks[1] , 'licenseState':chunks[2], 'zipCode':chunks[3]})
         return d
     elif chunks[0] == 'MAIL':
@@ -83,25 +112,33 @@ def convcfg(data):
         d.update({'loginType': 'transponder', 'loginNumber':chunks[1], 'zipCode':chunks[3]})
         return d
     elif chunks[0] == 'LIC':
-        d.update({'loginType': 'license', 'loginNumber':chunks[1] , 'licenseState':chunks[2], 'zipCode':chunks[3]})
+        d.update({'loginType': 'plate', 'loginNumber':chunks[1] , 'licenseState':chunks[2], 'zipCode':chunks[3]})
         return d
     else:
         raise Exception("Error While Reading Configuration!! Check for blank lines or incorrect line structure!")
 
 
-def get_errflash(soup):
-    ''' Retrieve the error box flash alert content '''
+def get_errflash(content):
+    ''' Retrieve the error flash alert text that appears at top of webpage and provids info hints '''
 
-    errflash = soup.form.div.table.tbody.td.table.tr.td.table.td.next.next.next.text
+    # In future work in better alternative for multiple errflash messages with soup.find_all("td", "data")
+    soup = bs4.BeautifulSoup(content.text, features='lxml')
 
+    try:
+       errflash = None
+       errflash = soup.form.div.table.tbody.td.table.tr.td.table.td.next.next.next.text
+    except AttributeError:
+        pass
+
+    # Test if the existence of an errflash is there and return it. Otherwise return False.
     if errflash:
         return errflash
     else:
-        return False
+        return None
 
 
-def find_token(req):
-    soup = bs4.BeautifulSoup(req.text)
+def find_token(content):
+    soup = bs4.BeautifulSoup(content.text, features='lxml')
     try:
         token = soup.form.input.attrs['value']
         return token
@@ -109,26 +146,90 @@ def find_token(req):
         pass
 
 
-def dispatchemail(item):
+def get_totals(webpage):
+    soup = bs4.BeautifulSoup(webpage.content, features='lxml')
+
+    # Should retrieve spans for Total Amount due and Total Number of Notices
+    overall_totals = soup.select("td > span")
+    # Should retrieve anchor links withh spans for each notice
+    violation_notices = soup.select("td.cl > a span")
+
+    total_amt = overall_totals[0].text + overall_totals[1].text
+    total_bills = overall_totals[3].text.replace('\xa0', '').replace('\n', ' ')
+
+    print("\t|------ Found Totals ------|")
+    print("\t\t",total_amt)
+    print("\t\t",total_bills)
+    print("\t|------  End Totals  ------|")
+    print('\n')
+
+
+def check_endable(message):
+    ''' Check errflash message against known end-of-operation messages. End program gracefully if required. '''
+
+    # Following should be benign indications that we can end the program
+    endable = [
+                "Multiple sessions have been detected. For your protection we are logging out of all sessions.",
+                "Your query has not returned any results.",
+                "No Data Matched the Search Criteria"
+               ]
+
+
+    # Not used.
+    # Following should be indicator that the program needs to be fixed or the configuration information is wrong.
+    recoverable = [
+                "Please fix the errors below.",
+                "Please Select License Plate State",
+                "Please enter either Transponder Number or License Plate",
+                "Please enter 13 digit Violation Number",
+                "Please enter valid Zip Code"
+                ]
+
+    print(f"Error Flash Messagebox Content: {message}")
+
+    if message in endable:
+        print("Error Flash Message is designated as endable. ")
+        return True
+    else:
+        print("Error Flash Message not designated to be endable. Continuing program execution.")
+        return False
+
+
+
+
+def get_pdf_page(link):
+    # probably should post to /vector/violations/violTransactionList.do?violationNumber=<T082023063482>
+    pass
+
+
+def dispatchemail(item, content):
     pass
 
 
 page_resp = request('GET', URL)
-
-# if check_resp call
+check_resp(page_resp)
 
 formtoken = find_token(page_resp)
 items = readstore()
 
 for item in items:
-    if (item.lstrip.startswith('#')) or (not item):
-        continue
-    print("Checking config for --> {}".format(item)) 
+
+    print(f"\tChecking config for --> {item} \n")
     page_resp = search_violation(convcfg(item))
-   # check_resp call
-   # Retrieve the number of billables
-   # Maybe alternatively setup a page on webserver for viewing for up to 48hrs if BILLABLES exist
-    dispatchemail(item, page_resp)
+
+    # Check for an error flash message on the page.
+    check_resp(page_resp)
+    message = get_errflash(page_resp)
 
 
-print("Script Ended.")
+    if check_endable(message):
+        continue
+    else:
+        get_totals(page_resp)
+
+    # Maybe alternatively setup a page on webserver for viewing for up to 48hrs if BILLABLES exist
+    #dispatchemail(item, page_resp)
+
+
+
+print("Script Ended. Godspeed.")
