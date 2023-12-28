b566cb32b91a50bced2b9413bafca09a39fc68ef
kali <kali@kali>
2023-12-28 13:33:10 -0500

    Preliminarily add likely imports

---
+++ nimpassmd/nimpassmd.nim
@@ -0,0 +1,10 @@
+
+import random
+import times
+import strformat
+import strutils
+import json
+import std/httpclient
+import std/logging
+#import std/smtp       # For some reason theyre forcing you to use a compiled with SSL in this language as if.. :eyeroll 
+
