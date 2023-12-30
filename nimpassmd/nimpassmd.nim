981137a2cd5ea8ce64c6849d251f6b9a87000035
kali <kali@kali>
2023-12-30 06:02:32 -0500

    Add sequtils

--- nimpassmd/nimpassmd.nim
+++ nimpassmd/nimpassmd.nim
@@ -6,5 +6,5 @@ import strutils
 import json
 import std/httpclient
 import std/logging
+import std/sequtils
 #import std/smtp       # For some reason theyre forcing you to use a compiled with SSL in this language as if.. :eyeroll 
-
